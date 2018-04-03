b7aae29758666eff331681bdba7af4831084ea01
zach <placeholder@gmail.com>
2018-04-03 10:52:11 -0400

    added basic logic for random forest

--- Untitled.R
+++ Untitled.R
@@ -4,9 +4,26 @@ library(randomForest)
 # docs at https://www.rdocumentation.org/packages/randomForest/versions/4.6-12
 
 # load the data set
+studentData <- loadData()
 
 # create the forest
+studentForest <- randomForest(
+  x = predictors,
+  data = studentData,
+  ntree = 500,
+  importance = TRUE,
+  type = classification
+)
 
 # view the forest results 
+print(studentForest)
 
 # importance of each predictor
+# type = 1 for mean decrease in accuracy, type = 2 for mean decrease impurity
+print(importance(fit, type = 2))
+
+# data set read function
+loadData <- function() {
+  # change logic for loading from database
+  
+}
